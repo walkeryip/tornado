ea83b8254e08e273dd3fd6e3370b2cec7b1a7502
rasmushaglund <rasmus.haglund@gmail.com>
2013-02-03 23:41:54 +0100

    New version of the db structure

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1
+-- version 3.4.11.1deb1
 -- http://www.phpmyadmin.net
 --
--- Värd: localhost
--- Skapad: 14 jan 2013 kl 19:37
--- Serverversion: 5.0.96
--- PHP-version: 5.2.9
+-- Host: localhost
+-- Generation Time: Feb 03, 2013 at 11:41 PM
+-- Server version: 5.5.28
+-- PHP Version: 5.4.6-1ubuntu1.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,182 +16,181 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE `tornado` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `tornado`;
+--
+-- Database: `tornado`
+--
+
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `contexts`
+-- Table structure for table `contexts`
 --
 
 CREATE TABLE IF NOT EXISTS `contexts` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` text character set latin1 NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET latin1 NOT NULL,
   `created` date NOT NULL,
   `user_id` int(11) NOT NULL,
   `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=35 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=34 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `contexts_tasks`
+-- Table structure for table `contexts_tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `contexts_tasks` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `context_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=616 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=793 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `contexts_task_lists`
+-- Table structure for table `contexts_task_lists`
 --
 
 CREATE TABLE IF NOT EXISTS `contexts_task_lists` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `context_id` int(11) NOT NULL,
   `task_list_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=93 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=212 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `tags`
+-- Table structure for table `tags`
 --
 
 CREATE TABLE IF NOT EXISTS `tags` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` text character set latin1 NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET latin1 NOT NULL,
   `created` date NOT NULL,
   `user_id` int(11) NOT NULL,
   `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=52 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=81 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `tags_tasks`
+-- Table structure for table `tags_tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `tags_tasks` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=743 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=942 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `tags_task_lists`
+-- Table structure for table `tags_task_lists`
 --
 
 CREATE TABLE IF NOT EXISTS `tags_task_lists` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_list_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=110 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=219 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `tasks`
+-- Table structure for table `tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` text character set latin1 NOT NULL,
-  `description` text character set latin1 NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET latin1 NOT NULL,
+  `description` text CHARACTER SET latin1 NOT NULL,
   `checked` tinyint(1) NOT NULL,
-  `deadline` date default NULL,
-  `priority` int(11) default NULL,
+  `deadline` date DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
   `created` date NOT NULL,
-  `todo` tinyint(1) NOT NULL default '1',
+  `todo` tinyint(1) NOT NULL DEFAULT '1',
   `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=733 ;
+  `energy` int(11) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=886 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `tasks_users`
+-- Table structure for table `tasks_users`
 --
 
 CREATE TABLE IF NOT EXISTS `tasks_users` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `read` tinyint(1) NOT NULL,
-  `write` tinyint(1) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=726 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1109 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `task_lists`
+-- Table structure for table `task_lists`
 --
 
 CREATE TABLE IF NOT EXISTS `task_lists` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
   `created` date NOT NULL,
-  `parent_id` int(11) default NULL,
+  `parent_id` int(11) DEFAULT NULL,
   `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=86 ;
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `task_lists_tasks`
+-- Table structure for table `task_lists_tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `task_lists_tasks` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `task_list_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=746 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=653 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `task_lists_users`
+-- Table structure for table `task_lists_users`
 --
 
 CREATE TABLE IF NOT EXISTS `task_lists_users` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_list_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `read` tinyint(1) NOT NULL,
-  `write` tinyint(1) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=93 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=306 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `deleted` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
